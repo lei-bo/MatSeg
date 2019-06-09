c0bbac5a03e07b5f1994100d198a4bc5f446bd4d
leibo <blei1@andrew.cmu.edu>
2019-06-08 23:37:33 -0400

    add recorder and metrics functions for training and evaluation

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+import torchvision.transforms as T
 
 
 class AverageMeter(object):
@@ -20,9 +22,17 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
-    def __str__(self):
-        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
-        return fmtstr.format(**self.__dict__)
+
+class Recorder(object):
+    def __init__(self, names):
+        self.names = names
+        self.record = {}
+        for name in self.names:
+            self.record[name] = []
+
+    def update(self, vals):
+        for name, val in zip(self.names, vals):
+            self.record[name].append(val)
 
 
 def generate_rand_ind(labels, n_class, n_samples):
@@ -42,4 +52,38 @@ def accuracy(predictions, labels):
     correct = predictions.eq(labels.cpu()).sum().item()
     acc = correct/np.prod(labels.shape)
     return acc
+
+
+def get_transform(size):
+    transform_img = T.Compose([
+        T.Resize(size),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    transform_label = T.Compose([
+        T.Resize(size),
+        T.ToTensor()
+    ])
+    return transform_img, transform_label
+
+
+def metrics(conf_mat, verbose=True):
+    c = conf_mat.shape[0]
+    # ignore dividing by zero
+    np.seterr(divide='ignore', invalid='ignore')
+    precision = conf_mat.diagonal()/conf_mat.sum(0)
+    recall = conf_mat.diagonal()/conf_mat.sum(1)
+    IoUs = np.zeros(c)
+    union_sum = 0
+    for i in range(c):
+        union = conf_mat[i,:].sum()+conf_mat[:,i].sum()-conf_mat[i,i]
+        union_sum += union
+        IoUs[i] = conf_mat[i,i]/union
+    acc = conf_mat.diagonal().sum()/conf_mat.sum()
+    IoU = IoUs.mean()
+    if verbose:
+        print('precision:', np.round(precision, 5))
+        print('recall:', np.round(recall, 5))
+        print('IoUs:', np.round(IoUs, 5))
+    return acc, IoU
 
