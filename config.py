93f1f5a3e632cc6a267389181b587e2e6c629634
leibo <blei1@andrew.cmu.edu>
2019-06-08 23:48:42 -0400

    update for defensive programming

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 # configurations
+import sys
 
 
 # generate default configuration given necessary infos and hyperparameters
 def gen_default(dataset, n_class, size, batch_size=1, lr=1e-4,
-                epoch=30, decay_epoch=(16,), print_freq=50):
+                epoch=60, print_freq=50):
     default = {
         'root': './data/' + dataset,
         'n_class': n_class,
         'size': size,
         'batch_size': batch_size,
+        'optimizer': 'Adam',
         'lr': lr,
         'epoch': epoch,
-        'decay_epoch': decay_epoch,
         'print_freq': print_freq
     }
     return default
@@ -20,23 +21,26 @@ def gen_default(dataset, n_class, size, batch_size=1, lr=1e-4,
 config = {
     'uhcs': {
         'default': gen_default('uhcs', n_class=4, size=(484, 645)),
-        'v1': {'model': 'pixelnet'}
-    },
-    'ECCI': {
-        'default': gen_default('ECCI', n_class=2, size=(768, 1024)),
-        'v1': {'model': 'pixelnet'}
+        'v1': {'model': 'pixelnet'},
+        'v2': {'model': 'unet'},
+        'v3': {'model': 'segnet'}
     },
     'tomography': {
         'default': gen_default('tomography', n_class=2, size=(852, 852)),
-        'v1': {'model': 'pixelnet'},
-        'v2': {'model': 'unet'}
+        'v1': {'model': 'pixelnet',  'batch_size': 1},
+        'v2': {'model': 'unet'},
+        'v3': {'model': 'segnet'}
     }
 }
 
 
 # combine the default config with version config
 def get_config(dataset, version):
-    args = config[dataset]['default'].copy()
+    try:
+        args = config[dataset]['default'].copy()
+    except KeyError:
+        print('dataset %s does not exist' % dataset)
+        sys.exit(1)
     args.update(config[dataset][version])
     args['name'] = dataset + '_' + version
     return args
