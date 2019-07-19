6a6fa71e92b903f0733d344e68afd5a3b1c689fd
leibo <blei1@andrew.cmu.edu>
2019-07-19 16:24:54 -0500

    Add mean and std of dataset. Add data augmentation flag.

--- config.py
+++ config.py
@@ -3,32 +3,43 @@ import sys
 
 
 # generate default configuration given necessary infos and hyperparameters
-def gen_default(dataset, n_class, size, batch_size=1, lr=1e-4,
-                epoch=60):
+def gen_default(dataset, n_class, size, mean, std, batch_size=1, lr=1e-4,
+                epoch=150):
     default = {
         'root': './data/' + dataset,
         'n_class': n_class,
         'size': size,
+        'mean': mean,
+        'std': std,
         'batch_size': batch_size,
         'optimizer': 'Adam',
         'lr': lr,
         'epoch': epoch,
+        'aug': False
     }
     return default
 
 
 config = {
     'uhcs': {
-        'default': gen_default('uhcs', n_class=4, size=(484, 645)),
+        'default': gen_default('uhcs', n_class=4, size=(484, 645),
+                               mean=[0.449, 0.449, 0.449], std=[0.168, 0.168, 0.168]),
         'v1': {'model': 'pixelnet'},
         'v2': {'model': 'unet'},
-        'v3': {'model': 'segnet'}
+        'v3': {'model': 'segnet', 'optimizer': 'SGD', 'lr': 0.01},
+        'v4': {'model': 'pixelnet', 'aug': True},
+        'v5': {'model': 'unet', 'aug': True},
+        'v6': {'model': 'segnet', 'aug': True, 'optimizer': 'SGD', 'lr': 0.01}
     },
     'tomography': {
-        'default': gen_default('tomography', n_class=2, size=(852, 852)),
+        'default': gen_default('tomography', n_class=2, size=(852, 852),
+                               mean=[0.504, 0.504, 0.504], std=[0.051, 0.051, 0.051]),
         'v1': {'model': 'pixelnet'},
         'v2': {'model': 'unet'},
-        'v3': {'model': 'segnet'}
+        'v3': {'model': 'segnet', 'optimizer': 'SGD', 'lr': 0.01},
+        'v4': {'model': 'pixelnet', 'aug': True},
+        'v5': {'model': 'unet', 'aug': True},
+        'v6': {'model': 'segnet', 'aug': True, 'optimizer': 'SGD', 'lr': 0.01}
     }
 }
 
