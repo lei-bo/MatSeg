297e2defd824edea39fa6b2a258b33c733ed4e6d
leibo <blei1@andrew.cmu.edu>
2019-10-11 15:27:44 -0400

    Add feature to set random seed.

--- main.py
+++ main.py
@@ -28,12 +28,13 @@ class Dataset(data.Dataset):
         self.size = size
         self.transform_img = transform_img
         self.transform_label = transform_label
+        self.seeds = [np.random.randint(np.iinfo(np.int32).max) for _ in range(len(self.img_names))]
 
     def __getitem__(self, index):
         img_name = self.img_names[index]
         img_dir = self.img_root + img_name
         label_dir = '%s%s.npy' % (self.label_root, img_name[:-4])  # the name of the label numpy file
-        seed = np.random.randint(2147483647) # get a random seed for fixed random transformation
+        seed = self.seeds[index] # get a random seed for fixed random transformation
         with open(img_dir, 'rb') as f:
             img = Image.open(f)
             img = img.convert('RGB')
@@ -119,11 +120,15 @@ def evaluate(config, model, criterion, validation_loader, method, test_flag=Fals
             print('--- validation result ---')
         conf_mat = conf_meter.value()
         acc, iou = metrics(conf_mat, verbose=test_flag)
-        print('loss: %.5f, accuracy: %.5f, mIU: %.5f' % (losses.avg, acc, iou))
+        print('loss: %.5f, accuracy: %.5f, IU: %.5f' % (losses.avg, acc, iou))
     return losses.avg, acc, iou
 
 
 def main(args):
+    if args.seed:
+        np.random.seed(int(args.seed))
+        torch.backends.cudnn.deterministic = True
+        torch.manual_seed(0)
     config = get_config(args.dataset, args.version)
     method = config['model']
     criterion = nn.CrossEntropyLoss().cuda()
@@ -133,8 +138,8 @@ def main(args):
         print('%s model does not exist' % method)
         sys.exit(1)
 
+    model_dir = './saved/%s_%s.pth' % (config['name'], method)
     if args.mode == 'train':
-        model_dir = './saved/%s_%s.pth' % (config['name'], method)
         log_dir = './log/%s_%s.log' % (config['name'], method)
         train_loader, validation_loader = get_dataloader(config)
         if config['optimizer'] == 'Adam':
@@ -172,7 +177,6 @@ def main(args):
         test_dir = '%s/%s' % (config['root'], args.test_folder)
         test_set = Dataset(test_dir, config['size'], *get_transform(config, is_train=False))
         test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=0, drop_last=False)
-        model_dir = './saved/%s_%s.pth' % (config['name'], method)
         model.load_state_dict(torch.load(model_dir)['model_state_dict'])
 
         # save prediction results, make directory if not exists
@@ -191,9 +195,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Micrograph Segmentation')
     parser.add_argument('dataset', help='name of the dataset folder')
     parser.add_argument('mode', choices=['train', 'evaluate'],
-                        help='mode choices: train, validate, test')
+                        help='mode choices: train, evaluate')
     parser.add_argument('version', help='version defined in config.py (v1, v2, ...)')
     parser.add_argument('--save', action='store_true', help='save the trained model')
     parser.add_argument('--test-folder', default='test', help='name of the folder running test')
+    parser.add_argument('--seed', default=None, help='random seed to reproduce same results')
     args = parser.parse_args()
     main(args)
