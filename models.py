90d8d8ef4f9c902882418c15afb0faaf99f9a1ec
leibo <blei1@andrew.cmu.edu>
2021-01-13 16:39:32 -0600

    clean up PixelNet model

--- models.py
+++ models.py
@@ -11,10 +11,10 @@ class PixelNet(torch.nn.Module):
         features = list(vgg16(pretrained=True).features)
         self.features = nn.ModuleList(features)
         self.classifier = nn.Sequential(
-            nn.Linear(1472, 2048, bias=True),
-            nn.ReLU(True),
+            nn.Linear(1472, 64, bias=True),
+            nn.ReLU(False),
             nn.Dropout(),
-            nn.Linear(2048, n_classes, bias=True),
+            nn.Linear(64, n_classes, bias=True),
         )
         self.linear = nn.Linear(1472, n_classes, bias=True)
         self.is_train = False
@@ -54,21 +54,18 @@ class PixelNet(torch.nn.Module):
             outputs = self.classifier(outputs)
             outputs = outputs.permute(0, 2, 1)
         else:
-            size, size_prod = (x.size(2), x.size(3)), x.size(2)*x.size(3)
-            feature_maps = []
+            size, n_pixels = (x.size(2), x.size(3)), x.size(2)*x.size(3)
+            upsample_maps = []
             for ii, model in enumerate(self.features):
                 x = model(x)
                 if ii in feature_maps_index:
-                    feature_maps.append(x)
+                    upsample = F.interpolate(x, size=size, mode='bilinear', align_corners=True)
+                    upsample_maps.append(upsample)
             outputs = []
-            for ind in range(0, size_prod, 10000):
-                if ind + 10000 > size_prod:
-                    ind_range = range(ind, size_prod)
-                else:
-                    ind_range = range(ind, ind+10000)
+            for ind in range(0, n_pixels, 10000):
+                ind_range = range(ind, min(ind+10000, n_pixels))
                 features = []
-                for map in feature_maps:
-                    upsample = F.interpolate(map, size=size, mode='bilinear', align_corners=True)
+                for upsample in upsample_maps:
                     upsample = upsample.view(upsample.size(0), upsample.size(1), -1)[:, :, ind_range]
                     features.append(upsample)
                 output = torch.cat(features, 1)
